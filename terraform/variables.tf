85907d2d1f970ce7584048a0642579e7c4f42214
Rowan Wilson <row@rowstu.net>
2026-02-03 10:14:20 +0000

    deploy draft 6

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,7 +8,7 @@ variable "storage" {
 
 variable "template_vm" {
   description = "Name of the Proxmox VM template to clone"
-  default = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  default     = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
 }
 
 variable "pm_api_url" {}
